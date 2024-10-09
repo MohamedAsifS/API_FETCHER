747833186a8681f640417110e160f90dd652b0bf
MAASIFAR <maasifar@gmail.com>
2024-10-09 23:08:38 +0530

    for arun

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 api_key = {
     "pixabay": "46218998-2ceeb9224ed3548d2618c757f",
     "pexels": "3CAbImegKz2Rb9YhSfOpJ9fLNHRG3x25C5idvisk2otyUbURHiSX1KVQ",
-    "unsplash": "6ieqbTayrqHcy9TnGDjG9FIaN3DKid-vT60oj1_klOk"
+    "unsplash": "6ieqbTayrqHcy9TnGDjG9FIaN3DKid-vT60oj1_klOk",
+    "Arun":"computer"
 }
 
 @app.route('/api', methods=['GET'])
