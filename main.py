d0ed8badc00e587aed73195c576ede3625dcdee9
MAASIFAR <maasifar@gmail.com>
2024-10-09 22:56:27 +0530

    again

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
-from flask import Flask,jsonify,request
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-api_key={
-    "pixabay":"46218998-2ceeb9224ed3548d2618c757f",
-    "pexels":"3CAbImegKz2Rb9YhSfOpJ9fLNHRG3x25C5idvisk2otyUbURHiSX1KVQ",
-    "unsplash":"6ieqbTayrqHcy9TnGDjG9FIaN3DKid-vT60oj1_klOk"
+# API keys dictionary
+api_key = {
+    "pixabay": "46218998-2ceeb9224ed3548d2618c757f",
+    "pexels": "3CAbImegKz2Rb9YhSfOpJ9fLNHRG3x25C5idvisk2otyUbURHiSX1KVQ",
+    "unsplash": "6ieqbTayrqHcy9TnGDjG9FIaN3DKid-vT60oj1_klOk"
 }
 
-@app.route('/get-user/<key>')
-def get_user(key):
-   
-    
-    
-    
+@app.route('/api', methods=['GET'])
+def get_user():
+    # Extract 'key' from query parameters
+    key = request.args.get('key')
+
     if key in api_key:
-        return jsonify(api_key[key]),200
+        return jsonify({"api_key": api_key[key]}), 200
     else:
-        return jsonify({"error":"Invalid API key"}),401
-    
-   
-
-    
-    
-    
-
-
-
-
-
-
+        return jsonify({"error": "Invalid API key"}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
